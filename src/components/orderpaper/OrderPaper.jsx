c36095568e21a410d6f803bab9bf0be37885d62e
Vatsal-yo <vatsalgoyal.07@gmail.com>
2026-08-31 13:05:24 +0530

    feat(orderpaper): move the tracked legislatures into a data module

--- src/components/orderpaper/OrderPaper.jsx
+++ src/components/orderpaper/OrderPaper.jsx
@@ -1,4 +1,5 @@
 import '../../styles/orderpaper.css'
+import { orderPaperItems } from '../../data/orderPaperItems';
 
 export default function OrderPaper() {
   return (
@@ -17,22 +18,12 @@ export default function OrderPaper() {
           <div id="opNote">Added &mdash; the paper posts each Friday, 09:00 GMT.</div>
         </div>
         <div className="op-list reveal">
-          <div className="op-item"><b>United Nations</b><span>General Assembly</span></div>
-          <div className="op-item"><b>Congress of the United States</b><span>Washington</span></div>
-          <div className="op-item"><b>House of Commons</b><span>Westminster</span></div>
-          <div className="op-item"><b>Rajya Sabha</b><span>New Delhi</span></div>
-          <div className="op-item"><b>European Parliament</b><span>Strasbourg</span></div>
-          <div className="op-item"><b>Folketing</b><span>Copenhagen</span></div>
-          <div className="op-item"><b>Bundestag</b><span>Berlin</span></div>
-          <div className="op-item"><b>Sejm</b><span>Warsaw</span></div>
-          <div className="op-item"><b>National Assembly</b><span>Paris</span></div>
-          <div className="op-item"><b>Riksdag</b><span>Stockholm</span></div>
-          <div className="op-item"><b>Knesset</b><span>Jerusalem</span></div>
-          <div className="op-item"><b>Cortes Generales</b><span>Madrid</span></div>
-          <div className="op-item"><b>Althingi</b><span>Reykjav&iacute;k</span></div>
-          <div className="op-item"><b>Oireachtas</b><span>Dublin</span></div>
-          <div className="op-item"><b>Senate of the Republic</b><span>Rome</span></div>
-          <div className="op-item"><b>House of Representatives</b><span>Canberra</span></div>
+          {orderPaperItems.map((it) => (
+            <div className="op-item" key={it.name}>
+              <b>{it.name}</b>
+              <span>{it.city}</span>
+            </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/data/orderPaperItems.js
@@ -0,0 +1,67 @@
+export const orderPaperItems =
+[
+  {
+    "name": "United Nations",
+    "city": "General Assembly"
+  },
+  {
+    "name": "Congress of the United States",
+    "city": "Washington"
+  },
+  {
+    "name": "House of Commons",
+    "city": "Westminster"
+  },
+  {
+    "name": "Rajya Sabha",
+    "city": "New Delhi"
+  },
+  {
+    "name": "European Parliament",
+    "city": "Strasbourg"
+  },
+  {
+    "name": "Folketing",
+    "city": "Copenhagen"
+  },
+  {
+    "name": "Bundestag",
+    "city": "Berlin"
+  },
+  {
+    "name": "Sejm",
+    "city": "Warsaw"
+  },
+  {
+    "name": "National Assembly",
+    "city": "Paris"
+  },
+  {
+    "name": "Riksdag",
+    "city": "Stockholm"
+  },
+  {
+    "name": "Knesset",
+    "city": "Jerusalem"
+  },
+  {
+    "name": "Cortes Generales",
+    "city": "Madrid"
+  },
+  {
+    "name": "Althingi",
+    "city": "Reykjav&iacute;k"
+  },
+  {
+    "name": "Oireachtas",
+    "city": "Dublin"
+  },
+  {
+    "name": "Senate of the Republic",
+    "city": "Rome"
+  },
+  {
+    "name": "House of Representatives",
+    "city": "Canberra"
+  }
+]
